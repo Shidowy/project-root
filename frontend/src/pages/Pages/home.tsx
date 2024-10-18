7de1cbfcd7305720dfe6c2ed5c8b420ba1984d04
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-10-18 15:42:59 -0700

    working

--- frontend/src/pages/Pages/home.tsx
+++ frontend/src/pages/Pages/home.tsx
@@ -3,14 +3,18 @@ import Hero from './hero';
 import ProductServices from './ProductServices';
 import SectionOne from './SectionOne';
 import CTA from './CTA';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
 const Home: React.FC = () => {
   return (
     <div className="home">
+      <NavBar/>
       <Hero />
       <ProductServices />
       <SectionOne />
       <CTA />
+      <Footer/>
     </div>
   );
 };
