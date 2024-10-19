8f0368ef73cbc63226a6906db2f7ad905d27536b
Shidowy <101477459+Shidowy@users.noreply.github.com>
2024-10-19 12:55:06 -0700

    test4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import Home from './pages/Home/home';
-import './app.css'
+import './App.css'
 
 function App() {
     const [message, setMessage] = useState<string>('');
